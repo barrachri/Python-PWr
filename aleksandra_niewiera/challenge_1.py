36773d5ea4b545b87e939a6f17e111a8b9a83b98
oniewiera <218427@student.pwr.edu.pl>
2017-05-06 17:44:23 +0200

    Challenge1 - Fix

--- aleksandra_niewiera/challenge_1.py
+++ aleksandra_niewiera/challenge_1.py
@@ -4,6 +4,7 @@ input_list = [1,2,3, None, 5]
 def api_call(a_list):
     '''This function takes a list of values and then it returns a integer
     with the number of numbers in that list'''
+    a_list=a_list
     counter=0
     for i in a_list:
         if type(i)==int:
