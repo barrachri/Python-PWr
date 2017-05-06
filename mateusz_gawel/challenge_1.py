bd5be2d6eee46acd598bf201e68c1710b31d3102
balu <mattegawel@gmail.com>
2017-05-06 17:35:46 +0200

    numcounter implementation

---
+++ mateusz_gawel/challenge_1.py
@@ -0,0 +1,13 @@
+input_list = [1,2,3, None, 5]
+
+def api_call(a_list):
+    '''This function takes a list of values and then it returns a integer 
+    with the number of numbers in that list'''
+    counter = 0
+    for something in input_list:
+        if type(something) == int:
+            counter = counter + 1
+
+    return counter
+
+assert api_call(input_list) == 4
