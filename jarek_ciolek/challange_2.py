957d8ff483ad76c2494bf829c359f6de5ab43fca
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2017-05-06 18:04:16 +0200

    Challange 2

---
+++ jarek_ciolek/challange_2.py
@@ -0,0 +1,17 @@
+import random
+input_list = [random.randint(-1,1) for i in range(100)]
+
+def api_call(input_list):
+    '''This function takes a list of values and then it returns a list with the
+    same number of elements [True, False,...] where
+    True if element >= 0
+    False if element < 0'''
+    
+    new_list = []
+    for i in input_list:
+    	if input_list[i] >= 0:
+    		new_list.append(True)
+    	else:
+    		new_list.append(False)
+    return new_list
+
