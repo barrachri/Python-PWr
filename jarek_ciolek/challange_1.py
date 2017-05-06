e5618913514f6f3ab0ccb17f708fad2fc8817108
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2017-05-06 17:47:10 +0200

    Challange 1

---
+++ jarek_ciolek/challange_1.py
@@ -0,0 +1,11 @@
+input_list = [1,2,3, None, 5]
+
+def api_call(a_list):
+    '''This function takes a list of values and then it returns a integer 
+    with the number of numbers in that list'''
+    counter = 0
+    for i in a_list:
+    	if type(i) == int:
+    		counter += 1
+    return counter
+assert api_call(input_list) == 4
